fc181c53e1cb98cdf6cb1195bb8bcaf744d13c9f
psycho <parthsaravade0@gmail.com>
2022-07-15 14:41:03 +0530

    init

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# mcserver-wo-portforwarding
-Sets up your own : minecraft server (without port forwarding (using ngrok)) + Discord Bot (to control it remotely).
+## mcserver-wo-portforwarding
+# About
+Simple node package : A discord bot to host and control a minecraft server, all without port forwarding.
+
+Usefull if your behind a NAT and your ISP doesnt allow port forwarding.
+
+This was written as I wanted to host my own minecraft server on an old pc in my grandma's house.
+
+## How it works
+It uses [ngrok](https://ngrok.com/) to open a tcp endpoint visible to the public. All incoming traffic is forwarded to the minecraft port 25565.
+# Installation
+## Prerequisites
+* Download and install [nodejs](https://nodejs.org/en/).
+* Make an [ngrok](https://ngrok.com/) account (It's free).
+* Setup a basic discord bot. [Tutorial part1](https://github.com/discordjs/guide/blob/main/guide/preparations/setting-up-a-bot-application.md) , [part2](https://github.com/discordjs/guide/blob/main/guide/preparations/adding-your-bot-to-servers.md).
+   > ! Enable Privileged Gateway Intents for your bot !
+* Download your minecraft server jar.
+
+## First time setup
+* git clone / Download this repository.
+* In the project directory run
+  ``` bash
+  npm install .
+  ```
+* Make a .env file containing your tokens
+  > .env
+    ``` 
+    AUTH_TOKEN = YOUR_NGROK_AUTH_TOKEN_HERE
+    BOT_TOKEN = YOUR_DISCORD_BOT_TOKEN_HERE
+    ```
+* Edit the index.js to include path to your server jar
+    ```javascript
+    11  jar: 'server.jar',
+    12  path: 'server/', // replace with path to your server jar
+    ```
+
+# Usage
+Run the bot using 
+  ``` bash
+  node index.js
+  ``` 
+## Bot Commands
+
+* !start : Starts the minecraft server and replies with the IP:PORT.
+* !stop : Stops the minecraft server.
+* !help : Displays help
+* !command \<Minecraft Command\> : Executes \<Minecraft Command\> on the server and replies with output.  
+# Node modules used
+This project was originally written as a bunch of bash scripts and a discord bot which would execute them. Recently I discovered that I could use node modules to have it all in node, also making it platform independent. 
+* [minecraft-java-server](https://github.com/joaisa17/minecraft-java-server)
+* [ngrok](https://github.com/bubenshchykov/ngrok)
+* [discord.js](https://github.com/discordjs/discord.js)
---
+++ index.js
@@ -0,0 +1,91 @@
+const { Client, Intents, ClientUser } = require('discord.js');
+require('dotenv').config();
+const ngrok = require('ngrok');
+var MinecraftServer = require ('minecraft-java-server');
+
+var ip;
+var is_server_on = false;
+// EDIT YOUR CUSTOM STUFF FROM HERE *******************************************
+const command_prefix = "!";
+const server = new MinecraftServer.MinecraftServer({
+    jar: 'server.jar',
+    path: 'server/', // replace with path to your server jar
+
+    args: ['-Xms4G', '-Xmx4G'],
+
+    // Minecraft's eula must be agreed to using this value
+    eula: true,
+    
+    // every property is the equivalent of server.properties, except for vital ones
+    properties: {
+        motd: "Minecraft server hosted with minecraft-java-server",
+        "max-players": 10,
+        //"online-mode": false
+    }
+});
+// EDIT YOUR CUSTOM STUFF TILL HERE *************************************************
+
+const client = new Client({
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES,Intents.FLAGS.GUILD_PRESENCES]
+  });
+
+client.on("ready", () => {
+    console.log("I am ready!");
+    client.user?.setPresence({ activities: [{ name: '!help',type: 'LISTENING' }], status: 'idle' });// Commands
+  });
+//client.user?.setActivity('!help', { type: 'LISTENING' });
+
+client.on('message', async message => {
+
+    if (message.author.bot) return;
+    if (!message.content.startsWith(command_prefix)) return;
+
+    const commandBody = message.content.slice(command_prefix.length);
+    const args = commandBody.split(' ');
+    const command = args.shift().toLowerCase();
+    if (command === "start") {
+	if(is_server_on){
+		message.reply('Server is already running');
+	}else{
+	        server.start();
+            await ngrok.authtoken(process.env.AUTH_TOKEN); 
+	        ip = await ngrok.connect({proto: 'tcp', addr: 25565});
+	        message.reply('Server Started ! Join at ' + ip.slice(6));
+            client.user?.setPresence({ activities: [{ name: ip.slice(6),type: 'PLAYING' }], status: 'online' });// Commands
+            is_server_on = true;
+	}
+    }
+    else if (command === "stop") {
+        if(is_server_on){
+		    server.stop();
+	        ngrok.disconnect(ip);
+	        message.reply('Server stopped !');
+            client.user?.setPresence({ activities: [{ name: '!help',type: 'LISTENING' }], status: 'idle' });// Commands
+            is_server_on = false;
+	}else{
+		message.reply('No server was running');
+	}
+    }
+    else if (command === "command")
+    {
+	if(is_server_on){
+		java_command = args.slice(0);
+        java_command = java_command.join(' ');
+        server.send(java_command).then(response => {
+		    message.reply(response);
+	            console.log('Command result: ', response);
+	        }).catch(err => console.log('Failed to run command', err));
+	}else{
+		message.reply('No server is running');
+	}
+    }
+    else if(command === "help")
+    {
+	message.channel.send('All commands start with \'!\' .');
+	message.channel.send('!start : starts the minecraft server \n!stop : stop the minecraft server \n!command <minecraft console command here> : runs command on server console and returns output ');
+    }
+
+});
+
+// Login to Discord with your client's token
+client.login(process.env.BOT_TOKEN);
